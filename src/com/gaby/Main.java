b6193c31d6f509dc9b3ec018b6d77276f5ffaae5
eyefloater <gobifish@gmail.com>
2015-11-23 15:08:08 -0600

    .

---
+++ src/com/gaby/Main.java
@@ -0,0 +1,27 @@
+/*You are a runner, and you are in training for a race. 
+You'd like to keep track of all of your times for your training runs. 
+You only like to run around lakes. Here's some example data,
+
+Calhoun, 45.15
+Calhoun, 43.32
+Harriet,  49.34
+Harriet, 44.43
+Harriet, 46.22
+Como, 32.11
+Como, 28.14
+Please write a program which enables you to enter the names of lakes and times, 
+and store it all of this data in data structure(s). 
+Don't store it in individual variables. 
+Your program should still work if you started running around another lake too (e.g. Cedar or Phalen).
+Your program should be able to analyze the data that you have stored, 
+and print your fastest time for each lake you ran around. So, for this data, your program will display
+Calhoun, 43.32
+Harriet, 44.43
+Como, 32.11*/
+
+
+package com.gaby;
+
+public class Main {
+
+}
